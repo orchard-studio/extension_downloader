cda08ae5c0f9cd9978444165dc14dd410684793d
Andrew Davis <andrew.davis@thinkorchard.com>
2014-04-03 18:04:32 +0100

    ending slash needed for all repos in xml file geturl var check selected exts

--- assets/extension_downloader.js
+++ assets/extension_downloader.js
@@ -147,8 +147,7 @@
 		} else {
 			searchTimer = setTimeout(search, 200);	
 		}
-	};
-	
+	};	
 	var resultClick = function (e) {
 		var t = $(this);
 		var handle = t.attr('data-handle');
@@ -186,39 +185,42 @@
 			win.scrollTop(tr.position().top);
 		}
 	};
-
+	var selected = function(){
+		$('tr.inactive.extension-can-install').each(function(){
+			$(this).addClass('selected');
+		});
+	}
+	function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+			vars[key] = value;
+		});
+		return vars;
+	}
 	var init = function () {
 		injectUI();
 		win.load(selectExtension);
-		selected();
-		
-		document.getElementById('xml_browser').addEventListener('change', handleFileSelect, false);
-
+		var a = getUrlVars()["a"];
+		if(a == '1'){
+			selected();
+		}
+		$('#xml_browser').on('change', handleFileSelect);
 		$('body').append("<iframe id='exportextensions' style='display:none'></iframe>");
-
+		$('#import_extensions').on('click', function(e) {
+			e.preventDefault();
+			$('#xml_browser').click();
+		});
 		$('#export_extensions').on('click',function(event){
-			event.preventDefault();	
-			
+			event.preventDefault();				
 			var arr = [];
 			$('.selected').each(function(){			
 				var id = $(this).find('td:nth-child(4)').find('a').attr('href');
 				arr.push(id);
 			});			
 			var text = arr.join(',');
-
 			arr =='' ? alert('Please select extensions to export') : $('#exportextensions').attr('src',EXPORT_URL +'?a='+text);
-
-		});
-		
-		
-		$('#import_extensions').on('click', function(e) {
-		   e.preventDefault();
-		  // Use the native click() of the file input.
-		  $('#xml_browser').click();
-
 		});
 	};
-
 	function handleFileSelect(evt) {
 
 		var files = evt.target.files; 
@@ -241,9 +243,7 @@
 					link.push(ext.attr('commit'));
 					var name = ext.attr('commit');
 					name = name.split('/');
-					//alert(name);
-					arr.push(' • \t' + name[4]);
-					//$('#list').after(ext.attr('href')+ '<br/>');					
+					arr.push(' • \t' + name[4]);									
 			});
 			link = link.join('!');
 			arr = arr.join('\n');
@@ -252,21 +252,12 @@
 						wrap.addClass('loading');
 				$('#xml_browser').attr('disabled', 'disabled').blur();	
 				$.post(UPLOAD_FILE,	{a : link},function(data){						
-						
-					if(data.success){
-						
-						alert('Download completed! Page will refresh.');
-						
-							document.location = EXTENSIONS_URL;
-							selected();
-						
-					} else if (data.exists) {
-						if (confirm('Extension ' + data.handle + ' already exists. Overwrite?')) {	
-							
-						}
-					}
+						alert('Download completed! Page will refresh.');						
+							document.location = EXTENSIONS_URL + '?a=1';
+							selected();					
 				}).fail(function(e){
-					alert('Import Complete');					
+					alert('Import Failed');	
+					document.location = EXTENSIONS_URL;					
 				}).always(function (e) {
 					wrap.removeClass('loading');
 					$('#xml_browser').removeAttr('disabled');
@@ -275,20 +266,9 @@
 			}
 		  }
 		};
-		
 		var blob = file.slice(start, stop + 1);
-		reader.readAsBinaryString(blob);
-		
-		
+		reader.readAsBinaryString(blob);		
 	  }
-	 
- 
-	var selected = function(){
-		$('tr.inactive.extension-can-install').each(function(){
-			$(this).addClass('selected');
-		});
-	}
-
 	$(init);
 	
 })(jQuery);
